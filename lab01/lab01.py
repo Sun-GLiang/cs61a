acd4ba260c3c3b480c6f30effc12cc7f7f0b227c
sgl <329162349@qq.com>
2023-12-27 13:06:20 +0800

    first

---
+++ lab01/lab01.py
@@ -0,0 +1,98 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    ans = 1
+    while k:
+        ans = ans * n
+        n = n - 1
+        k = k - 1
+    return ans
+
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 divisible by 7
+    >>> c
+    0
+    """
+    "*** YOUR CODE HERE ***"
+    cnt = 0
+    for x in range(1, n + 1):
+        if x % k == 0:
+            print(x)
+            cnt += 1
+    return cnt
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    length = len(str(y)) - 1
+    sum = 0
+    for i in range(0, length + 1):
+        tmp = y // 10 ** length
+        y -= tmp * 10 ** length
+        sum += tmp
+        length -= 1
+    return sum
+    
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    n = str(n)
+    for i in range(0, len(n) - 1):
+        j = i + 1
+        if n[i] == '8' and n[j] == '8':
+            return True
+    return False
+
